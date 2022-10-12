bc51b9e4df8bc1c9d88734fc5fa0337a530a07eb
Daniel S <danielsanchezvel@hotmail.com>
2022-10-11 23:14:27 -0500

    Header & About me were added

---
+++ frontend/src/components/SocialMedia.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+const SocialMedia = () => {
+  return <div>SocialMedia</div>;
+};
+
+export default SocialMedia;
