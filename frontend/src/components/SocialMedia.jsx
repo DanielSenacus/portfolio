f133c0738642f3b3c9a77ab46437f9fd7cc5d621
Daniel S <danielsanchezvel@hotmail.com>
2022-10-12 00:17:07 -0500

    wrapper added

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebook } from "react-icons/fa";
+
 const SocialMedia = () => {
-  return <div>SocialMedia</div>;
+  return (
+    <div className='app__social'>
+      <div>
+        <BsTwitter></BsTwitter>
+      </div>
+      <div>
+        <FaFacebook></FaFacebook>
+      </div>
+      <div>
+        <BsInstagram></BsInstagram>
+      </div>
+    </div>
+  );
 };
 
 export default SocialMedia;
