d6aa93b10e19028b0c3cdbfd8864416112eb3d1c
Daniel S <danielsanchezvel@hotmail.com>
2022-10-11 19:48:58 -0500

    navbar added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+
 import { images } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [toggle, setToggle] = React.useState(false);
   const links = ["home", "about", "work", "skills", "contact"];
 
   return (
@@ -19,6 +23,28 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)}></HiMenuAlt4>
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeInOut" }}
+          >
+            <HiX onClick={() => setToggle(false)}></HiX>
+            <ul>
+              {links.map((link) => (
+                <li key={link}>
+                  <a href={`#${link}`} onClick={() => setToggle(false)}>
+                    {link}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
